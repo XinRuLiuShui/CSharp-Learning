77a6e84a48b9bed93a120601c1cb9e08f7b2dc69
XinRuLiuShui <1677409816@qq.com>
2026-02-22 21:38:53 +0800

    设计了主界面UI

---
+++ 03_Main_UI/FormMain.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace _03_Main_UI
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.panelLeft = new System.Windows.Forms.Panel();
+            this.buttonChangePassword = new System.Windows.Forms.Button();
+            this.buttonSearchPainters = new System.Windows.Forms.Button();
+            this.buttonSearchPaintings = new System.Windows.Forms.Button();
+            this.buttonAddPainting = new System.Windows.Forms.Button();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.panelRight = new System.Windows.Forms.Panel();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.panelTop.SuspendLayout();
+            this.panelLeft.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelTop
+            // 
+            this.panelTop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(220)))), ((int)(((byte)(57)))));
+            this.panelTop.Controls.Add(this.buttonExit);
+            this.panelTop.Controls.Add(this.labelTitle);
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(1373, 71);
+            this.panelTop.TabIndex = 0;
+            // 
+            // panelLeft
+            // 
+            this.panelLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(238)))), ((int)(((byte)(156)))));
+            this.panelLeft.Controls.Add(this.buttonChangePassword);
+            this.panelLeft.Controls.Add(this.buttonSearchPainters);
+            this.panelLeft.Controls.Add(this.buttonSearchPaintings);
+            this.panelLeft.Controls.Add(this.buttonAddPainting);
+            this.panelLeft.Controls.Add(this.monthCalendar1);
+            this.panelLeft.Location = new System.Drawing.Point(3, 77);
+            this.panelLeft.Name = "panelLeft";
+            this.panelLeft.Size = new System.Drawing.Size(235, 679);
+            this.panelLeft.TabIndex = 1;
+            // 
+            // buttonChangePassword
+            // 
+            this.buttonChangePassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(225)))), ((int)(((byte)(87)))));
+            this.buttonChangePassword.FlatAppearance.BorderSize = 0;
+            this.buttonChangePassword.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(244)))), ((int)(((byte)(195)))));
+            this.buttonChangePassword.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(244)))), ((int)(((byte)(195)))));
+            this.buttonChangePassword.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonChangePassword.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonChangePassword.Image = global::_03_Main_UI.Properties.Resources.key;
+            this.buttonChangePassword.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonChangePassword.Location = new System.Drawing.Point(36, 601);
+            this.buttonChangePassword.Name = "buttonChangePassword";
+            this.buttonChangePassword.Size = new System.Drawing.Size(162, 53);
+            this.buttonChangePassword.TabIndex = 1;
+            this.buttonChangePassword.Text = "修 改 密 码";
+            this.buttonChangePassword.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonChangePassword.UseVisualStyleBackColor = false;
+            // 
+            // buttonSearchPainters
+            // 
+            this.buttonSearchPainters.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(225)))), ((int)(((byte)(87)))));
+            this.buttonSearchPainters.FlatAppearance.BorderSize = 0;
+            this.buttonSearchPainters.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(244)))), ((int)(((byte)(195)))));
+            this.buttonSearchPainters.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(244)))), ((int)(((byte)(195)))));
+            this.buttonSearchPainters.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSearchPainters.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonSearchPainters.Image = global::_03_Main_UI.Properties.Resources.artist;
+            this.buttonSearchPainters.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonSearchPainters.Location = new System.Drawing.Point(36, 366);
+            this.buttonSearchPainters.Name = "buttonSearchPainters";
+            this.buttonSearchPainters.Size = new System.Drawing.Size(162, 53);
+            this.buttonSearchPainters.TabIndex = 1;
+            this.buttonSearchPainters.Text = "   作 家 信 息";
+            this.buttonSearchPainters.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonSearchPainters.UseVisualStyleBackColor = false;
+            // 
+            // buttonSearchPaintings
+            // 
+            this.buttonSearchPaintings.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(225)))), ((int)(((byte)(87)))));
+            this.buttonSearchPaintings.FlatAppearance.BorderSize = 0;
+            this.buttonSearchPaintings.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(244)))), ((int)(((byte)(195)))));
+            this.buttonSearchPaintings.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(244)))), ((int)(((byte)(195)))));
+            this.buttonSearchPaintings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSearchPaintings.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonSearchPaintings.Image = global::_03_Main_UI.Properties.Resources.website_design;
+            this.buttonSearchPaintings.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonSearchPaintings.Location = new System.Drawing.Point(36, 290);
+            this.buttonSearchPaintings.Name = "buttonSearchPaintings";
+            this.buttonSearchPaintings.Size = new System.Drawing.Size(162, 53);
+            this.buttonSearchPaintings.TabIndex = 1;
+            this.buttonSearchPaintings.Text = "   作 品 信 息";
+            this.buttonSearchPaintings.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonSearchPaintings.UseVisualStyleBackColor = false;
+            // 
+            // buttonAddPainting
+            // 
+            this.buttonAddPainting.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(225)))), ((int)(((byte)(87)))));
+            this.buttonAddPainting.FlatAppearance.BorderSize = 0;
+            this.buttonAddPainting.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(244)))), ((int)(((byte)(195)))));
+            this.buttonAddPainting.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(244)))), ((int)(((byte)(195)))));
+            this.buttonAddPainting.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAddPainting.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonAddPainting.Image = global::_03_Main_UI.Properties.Resources.layer_plus__1_;
+            this.buttonAddPainting.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonAddPainting.Location = new System.Drawing.Point(36, 211);
+            this.buttonAddPainting.Name = "buttonAddPainting";
+            this.buttonAddPainting.Size = new System.Drawing.Size(162, 53);
+            this.buttonAddPainting.TabIndex = 1;
+            this.buttonAddPainting.Text = "   添 加 作 品";
+            this.buttonAddPainting.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonAddPainting.UseVisualStyleBackColor = false;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(6, 9);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            // 
+            // panelRight
+            // 
+            this.panelRight.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(238)))), ((int)(((byte)(156)))));
+            this.panelRight.Location = new System.Drawing.Point(263, 77);
+            this.panelRight.Name = "panelRight";
+            this.panelRight.Size = new System.Drawing.Size(1091, 679);
+            this.panelRight.TabIndex = 0;
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("微软雅黑", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelTitle.Image = global::_03_Main_UI.Properties.Resources.customize_computer__1_;
+            this.labelTitle.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.labelTitle.Location = new System.Drawing.Point(22, 18);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(285, 38);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "    艺术作品管理系统";
+            this.labelTitle.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(220)))), ((int)(((byte)(57)))));
+            this.buttonExit.FlatAppearance.BorderSize = 0;
+            this.buttonExit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(244)))), ((int)(((byte)(195)))));
+            this.buttonExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(244)))), ((int)(((byte)(195)))));
+            this.buttonExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonExit.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonExit.Location = new System.Drawing.Point(1301, 12);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(53, 38);
+            this.buttonExit.TabIndex = 1;
+            this.buttonExit.Text = "×";
+            this.buttonExit.UseVisualStyleBackColor = false;
+            this.buttonExit.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(244)))), ((int)(((byte)(195)))));
+            this.ClientSize = new System.Drawing.Size(1366, 768);
+            this.Controls.Add(this.panelRight);
+            this.Controls.Add(this.panelLeft);
+            this.Controls.Add(this.panelTop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormMain";
+            this.Text = "主界面";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Frm_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Frm_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Frm_MouseUp);
+            this.panelTop.ResumeLayout(false);
+            this.panelTop.PerformLayout();
+            this.panelLeft.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Panel panelLeft;
+        private System.Windows.Forms.Panel panelRight;
+        private System.Windows.Forms.Button buttonAddPainting;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Button buttonSearchPaintings;
+        private System.Windows.Forms.Button buttonSearchPainters;
+        private System.Windows.Forms.Button buttonChangePassword;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
+
---
+++ 03_Main_UI/FormMain.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _03_Main_UI
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        //无边框的WinForm窗体，拖动位置
+        private Point mouseOff;//鼠标移动位置变量
+        private bool leftFlag;//标签是否为左键
+        private void Frm_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)//判断鼠标左键是否被按下
+            {
+                mouseOff = new Point(-e.X, -e.Y);//得到变量的值
+                leftFlag = true;//标记鼠标左键的状态
+            }
+        }
+
+        private void Frm_MouseMove(object sender, MouseEventArgs e)//鼠标移动触发MouseMove事件
+        {
+            if (leftFlag)//判断鼠标左键有没有被按下
+            {
+                Point mouseSet = Control.MousePosition;//获取屏幕中鼠标所在的位置
+                mouseSet.Offset(mouseOff.X, mouseOff.Y);//设置移动后的位置
+                Location = mouseSet;
+            }
+        }
+
+        private void Frm_MouseUp(object sender, MouseEventArgs e)//鼠标左键抬起后触发MouseUp事件
+        {
+            if (leftFlag)
+            {
+                leftFlag = false;//释放鼠标后标注为false；
+            }
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
