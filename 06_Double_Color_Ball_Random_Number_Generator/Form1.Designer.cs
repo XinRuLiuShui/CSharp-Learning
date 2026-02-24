1d8b42314bd55dafabb3e0aea55a67a20efc0d43
XinRuLiuShui <1677409816@qq.com>
2026-02-24 21:29:33 +0800

    实现双色球随机选号器

---
+++ 06_Double_Color_Ball_Random_Number_Generator/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace _06_Double_Color_Ball_Random_Number_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(131, 300);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(69, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 60);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "00";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(135, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 60);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "00";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(201, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 60);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "00";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(267, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 60);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "00";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(333, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 60);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "00";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("微软雅黑", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(399, 88);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 60);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "00";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("微软雅黑", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.Location = new System.Drawing.Point(522, 88);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 60);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "00";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(364, 300);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 42);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "停止";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::_06_Double_Color_Ball_Random_Number_Generator.Properties.Resources.resized_640x480;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "双色球随机选号器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 06_Double_Color_Ball_Random_Number_Generator/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _06_Double_Color_Ball_Random_Number_Generator
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+
+        int rollCount = 0;      // 当前滚动次数
+        int maxRoll = 100;       // 最大滚动次数（20 * 50ms = 1秒）
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MakeCircle(Label lbl, Color color)
+        {
+            lbl.BackColor = color;
+
+            System.Drawing.Drawing2D.GraphicsPath path =
+                new System.Drawing.Drawing2D.GraphicsPath();
+
+            path.AddEllipse(0, 0, lbl.Width, lbl.Height);
+
+            lbl.Region = new Region(path);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //List<int> redBalls = new List<int>();
+
+            //while (redBalls.Count < 6)
+            //{
+            //    int num=random.Next(1, 34);
+
+            //    if (!redBalls.Contains(num))
+            //    {
+            //        redBalls.Add(num);
+            //    }
+            //}
+
+            //redBalls.Sort();
+
+            //int blueBall = random.Next(1, 17);
+
+            //string result = "红球:";
+
+            //foreach (int num in redBalls)
+            //{
+            //    result += num.ToString("D2") + " ";
+            //}
+
+            //result += "蓝球:" + blueBall.ToString("D2");
+
+            //textBox1.Text = result;
+
+            //// 显示红球
+            //label1.Text = redBalls[0].ToString("D2");
+            //label2.Text = redBalls[1].ToString("D2");
+            //label3.Text = redBalls[2].ToString("D2");
+            //label4.Text = redBalls[3].ToString("D2");
+            //label5.Text = redBalls[4].ToString("D2");
+            //label6.Text = redBalls[5].ToString("D2");
+
+            //label7.Text = blueBall.ToString("D2");
+
+            rollCount = 0;
+            timer1.Start();  // 启动动画
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MakeCircle(label1, Color.Red);
+            MakeCircle(label2, Color.Red);
+            MakeCircle(label3, Color.Red);
+            MakeCircle(label4, Color.Red);
+            MakeCircle(label5, Color.Red);
+            MakeCircle(label6, Color.Red);
+
+            MakeCircle(label7, Color.Blue);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //rollCount++;
+
+            // 每次都生成随机数
+            List<int> redBalls = new List<int>();
+
+            while (redBalls.Count < 6)
+            {
+                int num = random.Next(1, 34);
+                if (!redBalls.Contains(num))
+                    redBalls.Add(num);
+            }
+
+            redBalls.Sort();
+            int blueBall = random.Next(1, 17);
+
+            // 显示到球上
+            label1.Text = redBalls[0].ToString("D2");
+            label2.Text = redBalls[1].ToString("D2");
+            label3.Text = redBalls[2].ToString("D2");
+            label4.Text = redBalls[3].ToString("D2");
+            label5.Text = redBalls[4].ToString("D2");
+            label6.Text = redBalls[5].ToString("D2");
+
+            label7.Text = blueBall.ToString("D2");
+
+            // 到达最大次数停止
+            //if (rollCount >= maxRoll)
+            //{
+            //    timer1.Stop();
+            //}
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+    }
+}
